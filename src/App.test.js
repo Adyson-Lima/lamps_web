431f1bde48f1c9684c3b56331b4f4ada783ce4d3
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-23 16:50:21 -0300

    4° commit - tdd botão novo tela read delete: RED

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Exist card em Lamps/index?', () => {
+test('Existe card em Lamps/index?', () => {
   render(<App />);
   const linkElement = screen.getByText("Lamps Crud");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Existe botão novo em Lamps/index?', () => {
+  render(<App />);
+  const linkElement = screen.getByText("Novo");
   expect(linkElement).toBeInTheDocument();
 });
