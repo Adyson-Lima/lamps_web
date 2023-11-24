097bd8110d7de1cf96a4d3fe898e54de4bb95bf1
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-24 15:13:23 -0300

    12° commit - tdd card tela new update: RED

--- src/App.test.js
+++ src/App.test.js
@@ -1,32 +1,45 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Existe card em Lamps/index?', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Lamps Crud");
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+  render(<App/>);
+  });
+
+describe("Testes da tela Lamps/index", () => {
+
+  test('Existe card em Lamps/index?', () => {
+    const linkElement = screen.getByText("Lamps Crud");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('Existe botão novo em Lamps/index?', () => {
+    const linkElement = screen.getByText("Novo");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('Existe tabela em Lamps/index?', () => {
+    const tableElement = screen.getByRole("table");
+    expect(tableElement).toHaveClass("table");
+  });
+
+  test("Existe botão editar em Lamps/index?", () => {
+    const buttonElement = screen.getByText("Editar");
+    expect(buttonElement).toHaveClass("btn-outline-info");
+  });
+
+  test("Existe botão excluir em Lamps/index?", () => {
+    const buttonElement = screen.getByText("Excluir");
+    expect(buttonElement).toHaveClass("btn btn-outline-danger");
+  });
 });
 
-test('Existe botão novo em Lamps/index?', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Novo");
-  expect(linkElement).toBeInTheDocument();
+describe("Testes da tela NewUpdate/index", () => {
+
+  test("Existe card na tela NewUpdate/index?", () => {
+    const linkElement = screen.getByText("Lamps Cruds");
+    expect(linkElement).toBeInTheDocument();
+
+  });
+
 });
 
-test('Existe tabela em Lamps/index?', () => {
-  render(<App />);
-  const tableElement = screen.getByRole("table");
-  expect(tableElement).toHaveClass("table");
-});
-
-test("Existe botão editar em Lamps/index?", () => {
-  render(<App />);
-  const buttonElement = screen.getByText("Editar");
-  expect(buttonElement).toHaveClass("btn-outline-info");
-});
-
-test("Existe botão excluir em Lamps/index?", () => {
-  render(<App />);
-  const buttonElement = screen.getByText("Excluir");
-  expect(buttonElement).toHaveClass("btn btn-outline-danger");
-});
---
+++ src/pages/NewUpdate/index.js
@@ -0,0 +1,9 @@
+import {Link, useNavigate, useParams} from "react-router-dom";
+import React, {useState, udeEffect} from "react";
+// import api from "../../services/api";
+
+export default function NewUpdate() {
+  return(
+
+  );
+}
